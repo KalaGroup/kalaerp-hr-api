103d94c23cb2e683b9eaa59fe1e616fee92191b8
Nihalahmed Mulla <erp.developer5@kalabiz.com>
2025-08-12 17:31:25 +0530

    Task-RecruitmentAttributeMaster CRUD API

---
+++ KalaGenset.ERP.HR.Core/Request/RecruitmentAttributeMaster/InsertRecruitmentAttributeMasterRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaGenset.ERP.HR.Core.Request.RecruitmentAttributeMaster
+{
+    public class InsertRecruitmentAttributeMasterRequest
+    {
+        public string RecruitmentAttributeName { get; set; } = null!;
+
+        public int RecruitmentAttributeMarks { get; set; }
+
+        public string RecruitmentAttributeRemark { get; set; } = null!;
+
+        public string RecruitmentAttributeAuthRemark { get; set; } = null!;
+
+        public bool RecruitmentAttributeAuth { get; set; }
+
+        public bool RecruitmentAttributeIsDiscard { get; set; }
+
+        public bool RecruitmentAttributeIsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ KalaGenset.ERP.HR.Core/Request/RecruitmentAttributeMaster/UpdateRecruitmentAttributeMasterRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaGenset.ERP.HR.Core.Request.RecruitmentAttributeMaster
+{
+    public class UpdateRecruitmentAttributeMasterRequest
+    {
+        public int RecruitmentAttributeId { get; set; }
+
+        public string RecruitmentAttributeName { get; set; } = null!;
+
+        public int RecruitmentAttributeMarks { get; set; }
+
+        public string RecruitmentAttributeRemark { get; set; } = null!;
+
+        public string RecruitmentAttributeAuthRemark { get; set; } = null!;
+
+        public bool RecruitmentAttributeAuth { get; set; }
+
+        public bool RecruitmentAttributeIsDiscard { get; set; }
+
+        public bool RecruitmentAttributeIsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
