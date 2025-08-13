2abcd6965fb342f89a8ba1a20109da4d30c5543e
Nihalahmed Mulla <erp.developer5@kalabiz.com>
2025-08-13 18:29:22 +0530

    Task-EmployeeMasterUpdationForMaster(CRUD API Done)

--- KalaGenset.ERP.HR.API/Program.cs
+++ KalaGenset.ERP.HR.API/Program.cs
@@ -19,6 +19,7 @@ using KalaGenset.ERP.HR.Core.Request.Currency;
 using KalaGenset.ERP.HR.Core.Request.Department;
 using KalaGenset.ERP.HR.Core.Request.District;
 using KalaGenset.ERP.HR.Core.Request.DivisionMaster;
+using KalaGenset.ERP.HR.Core.Request.EmployeeMasterUpdationForMaster;
 using KalaGenset.ERP.HR.Core.Request.EmployeeTypeMaster;
 using KalaGenset.ERP.HR.Core.Request.Facility;
 using KalaGenset.ERP.HR.Core.Request.Grade;
@@ -44,6 +45,7 @@ using KalaGenset.ERP.HR.Core.Validation.DepartmentMaster;
 using KalaGenset.ERP.HR.Core.Validation.DepartmentValidation;
 using KalaGenset.ERP.HR.Core.Validation.DistrictMasterValidation;
 using KalaGenset.ERP.HR.Core.Validation.DivisionMasterValidation;
+using KalaGenset.ERP.HR.Core.Validation.EmployeeMasterUpdationForMasterValidation;
 using KalaGenset.ERP.HR.Core.Validation.EmployeeTypeMasterValidation;
 using KalaGenset.ERP.HR.Core.Validation.FacilityMaster;
 using KalaGenset.ERP.HR.Core.Validation.GradeValidation;
@@ -107,6 +109,8 @@ builder.Services.AddValidatorsFromAssemblyContaining<InsertActivityMasterValidat
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateActivityMasterValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<InsertRecruitmentAttributeMasterRequest>();
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateRecruitmentAttributeMasterRequest>();
+builder.Services.AddValidatorsFromAssemblyContaining<InsertEmployeeMasterUpdationForMasterRequest>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateEmployeeMasterUpdationForMasterRequest>();
 
 //registering service
 builder.Services.AddScoped<ICountryMaster, CountryMasterService>();
@@ -195,6 +199,9 @@ builder.Services.AddScoped<IActivityDetails, ActivityDetailsServices>();
 builder.Services.AddScoped<IValidator<InsertRecruitmentAttributeMasterRequest>, InsertRecruitmentAttributeMasterValidator>();
 builder.Services.AddScoped<IValidator<UpdateRecruitmentAttributeMasterRequest>, UpdateRecruitmentAttributeMasterValidator>();
 builder.Services.AddScoped<IRecruitmentAttributeMaster, RecruitmentAttributeMasterService>();
+builder.Services.AddScoped<IEmployeeMasterUpdationForMaster, EmployeeMasterUpdationForMasterService>();
+builder.Services.AddScoped<IValidator<InsertEmployeeMasterUpdationForMasterRequest>, InsertEmployeeMasterUpdationForMasterValidator>();
+builder.Services.AddScoped<IValidator<UpdateEmployeeMasterUpdationForMasterRequest>, UpdateEmployeeMasterUpdationForMasterValidator>();
 
 
 builder.Services.AddCors(options =>
