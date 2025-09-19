0492a61941b12232aacf77815f9dc887899751e8
Irfan Kalekar <erp.developer2@kalabiz.com>
2025-09-19 10:22:15 +0530

    districtmaster_api changes done

---
+++ KalaGenset.ERP.HR.Core/ResponseDTO/DistrictMaster/DistrictMasterDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaGenset.ERP.HR.Core.ResponseDTO.DistrictMaster
+{
+    public class DistrictMasterDTO
+    {
+        public int DistrictId { get; set; }
+
+        public string CountryName { get; set; } = null!;
+
+        public string StateName { get; set; } = null!;
+
+        public string DistrictCode { get; set; } = null!;
+
+        public string DistrictName { get; set; } = null!;
+
+        public string ShortName { get; set; } = null!;
+
+        public bool IsDiscard { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string DistrictMasterRemark { get; set; } = null!;
+
+        public string DistrictMasterAuthRemark { get; set; } = null!;
+
+        public bool DistrictMasterAuth { get; set; }
+    }
+}
