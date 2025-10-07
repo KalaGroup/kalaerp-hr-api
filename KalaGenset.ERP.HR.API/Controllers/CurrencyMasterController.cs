1eccc807ef6ded9e9a13e7ef048fad41251381ab
Admin_Vishnu <manish@kalabiz.com>
2025-10-07 15:01:48 +0530

    Changes for showing proper validation in company master, Country master, Currency master, Grade Master controller.

--- KalaGenset.ERP.HR.API/Controllers/CurrencyMasterController.cs
+++ KalaGenset.ERP.HR.API/Controllers/CurrencyMasterController.cs
@@ -39,7 +39,10 @@ namespace KalaGenset.ERP.HR.API.Controllers
             var validationResult = await _validator.ValidateAsync(request);
             if (!validationResult.IsValid)
             {
-                return BadRequest(validationResult.Errors);
+                var errors = validationResult.Errors
+                    .Select(e => new { field = e.PropertyName, message = e.ErrorMessage })
+                    .FirstOrDefault();
+                return BadRequest(errors);
             }
             try
             {
@@ -74,7 +77,10 @@ namespace KalaGenset.ERP.HR.API.Controllers
             var validationResult = await _updatevalidator.ValidateAsync(request);
             if (!validationResult.IsValid)
             {
-                return BadRequest(validationResult.Errors);
+                var errors = validationResult.Errors
+                   .Select(e => new { field = e.PropertyName, message = e.ErrorMessage })
+                   .FirstOrDefault();
+                return BadRequest(errors);
             }
             try
             {
